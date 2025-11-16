a0e9dac7ba6f137fa4590de9f6c93a1900c4e198
McNugget <alexd84a@gmail.com>
2025-11-16 14:03:06 +0100

    glurp

---
+++ Aoc2025/AdventUtility/Utility.cs
@@ -0,0 +1,21 @@
+﻿namespace AdventUtility;
+
+public static class Utility
+{
+    public static string ReadInput(bool test, int advent)
+    {
+        string adv = (advent < 10 ? "0" : "") + advent.ToString();
+
+        string path = Path.Combine(
+            Path.Combine(AppContext.BaseDirectory, test ? "Test" : "Input"),
+            $"input{adv}.txt"
+        );
+        return File.ReadAllText(path);
+    }
+
+    public static T[] ConvertTo<T>(this string input, char separator = '\n')
+    {
+        string[] x = input.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+        return x.Select(p => (T)Convert.ChangeType(p, typeof(T))).ToArray();
+    }
+}
